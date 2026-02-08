659a30cbad08b1dae081c2be6463e090e2135763
marcosdollis <marcosfelipedollis@gmail.com>
2026-02-07 21:54:37 -0300

    feat: Add CSRF_TRUSTED_ORIGINS configuration to settings

--- mediaexpand/settings.py
+++ mediaexpand/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)  # Default to True for development
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
