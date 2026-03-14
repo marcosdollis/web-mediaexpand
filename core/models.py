0b571ef39bad56f3e442a6c622a2a50689a804fd
marcosdollis <marcosfelipedollis@gmail.com>
2026-03-13 22:06:26 -0300

    .

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
+from django.utils import timezone
 import os
 import uuid
 import json
